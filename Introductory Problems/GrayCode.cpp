a031e20683ac31dba1b78d39e61122c349e3a72e
HOME <home@Adams-MacBook-Pro.local>
2024-09-06 22:42:46 +0700

    upsol

---
+++ Introductory Problems/GrayCode.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#define fi first
+#define se second
+#define el '\n'
+#define pb push_back
+#define eb emplace_back
+#define io() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mask(i) (1LL << (i))
+#define getbit(mask, i) (((mask) >> (i)) & 1)
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<ll> v64;
+typedef vector<vector<ll> > vv64;
+typedef vector<p32> vp32;
+typedef vector<vector<p32> > vvp32;
+typedef vector<p64> vp64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<bool> vb;
+typedef vector<vector<bool> > vvb;
+typedef vector<string> vs;
+typedef vector<vs > vvs;
+
+int pop_cnt(ll mask) { return __builtin_popcountll(mask); }
+int ctz(ull mask) { return __builtin_ctzll(mask); }
+int logOf(ull mask) { return 63 - __builtin_clzll(mask); }
+
+const ll mod = (ll)(1e9+7);
+const ll inf = (ll)(2e18);
+
+int n;
+v32 a(17);
+bool ok;
+
+void sol()
+{
+    cin >> n;
+    int k = 1 << n;
+    string s[k+5];
+    s[1] = "0", s[2] = "1";
+    if (n > 1)
+    {
+        int t = 2;
+        for (int i=2; i<=n; i++)
+        {
+            int l = 1 << i;
+            for (int j=1; j<=t; j++)
+            {
+                s[l-j+1] = "1" + s[j];
+                s[j] = "0" + s[j];
+            }
+            t = l;
+        }
+    }
+    for (int i=1; i<=k; i++) cout << s[i] << el;
+}
+
+int32_t main()
+{
+    io();
+    sol();
+    cerr << "\nTime elapsed: " << 1000*clock()/CLOCKS_PER_SEC << "ms\n";
+}
---
+++ Introductory Problems/PalindromeReorder.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#define fi first
+#define se second
+#define el '\n'
+#define pb push_back
+#define eb emplace_back
+#define io() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mask(i) (1LL << (i))
+#define getbit(mask, i) (((mask) >> (i)) & 1)
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<ll> v64;
+typedef vector<vector<ll> > vv64;
+typedef vector<p32> vp32;
+typedef vector<vector<p32> > vvp32;
+typedef vector<p64> vp64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<bool> vb;
+typedef vector<vector<bool> > vvb;
+typedef vector<string> vs;
+typedef vector<vs > vvs;
+
+int pop_cnt(ll mask) { return __builtin_popcountll(mask); }
+int ctz(ull mask) { return __builtin_ctzll(mask); }
+int logOf(ull mask) { return 63 - __builtin_clzll(mask); }
+
+const ll mod = (ll)(1e9+7);
+const ll inf = (ll)(2e18);
+
+void sol()
+{
+    string s; cin >> s;
+    string ans = "";
+    vb a(26, 0);
+    int cnt = 0;
+    for (auto c: s)
+    {
+        if (!a[c-'A']) a[c-'A'] = 1, cnt++;
+        else ans += c, a[c-'A'] = 0, cnt--;
+    }
+    if (cnt >= 2)
+    {
+        cout << "NO SOLUTION";
+        return;
+    }
+    cout << ans;
+    for (int i=0; i<26; i++)
+    {
+        if (a[i]) cout << char(i+'A');
+    }
+    reverse(all(ans));
+    cout << ans;
+}
+
+int32_t main()
+{
+    io();
+    sol();
+    cerr << "\nTime elapsed: " << 1000*clock()/CLOCKS_PER_SEC << "ms\n";
+}
